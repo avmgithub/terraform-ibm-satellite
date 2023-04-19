a253659283243ba4b5d2057d0bcc4b6e7fd0a371
avmgithub <avm@satoriconnections.com>
2023-04-19 09:27:37 -0500

    changes for satellite demo

--- modules/location/output.tf
+++ modules/location/output.tf
@@ -1,8 +1,12 @@
 
 output "location_id" {
-  value = data.ibm_satellite_location.location.id
+  value = try(data.ibm_satellite_location.location.id, "")
 }
 
 output "host_script" {
-  value = data.ibm_satellite_attach_host_script.script.host_script
+  value = try(data.ibm_satellite_attach_host_script.script.host_script, "")
+}
+
+output "script_path" {
+  value = try(data.ibm_satellite_attach_host_script.script.script_path, "")
 }
